fda2457387e665a2661e008bf94b93feead10a29
Eddy Struillou <eddy.s25@hotmail.fr>
2018-07-26 12:25:47 +0200

    Ajout async + eclatement en function des elements

--- src/scripts.js
+++ src/scripts.js
@@ -1,27 +1,34 @@
 const mainEl = document.getElementsByClassName('container')[0]
 
-//Request de notre fichier movies.json
-fetch('data/movie.json')
-  .then(response => {
-//    throw  new Error('ko') //Affiche une erreur directement
-    return response.json() //Retourne une réponse HTTP que l'on va convertir en json
-})
-.then(json => {
-//  console.log('Request successful', json)
-  json.films.forEach(movie => { //forEach de les data que l'on récupère
-    const newDiv = document.createElement('div')
-    const image = document.createElement('img')
-    const titreh4 = document.createElement('h4')
-    image.src = movie.imagesURL
-    titreh4.innerText = movie.title
-    mainEl.appendChild(newDiv)
-    newDiv.appendChild(image)
-    newDiv.appendChild(titreh4)
-    newDiv.onclick = afficheInfos.bind(movie)
-  })
-})
-.catch(error => {
+function init (movie) {
+  const newDiv = document.createElement('div')
+  const image = document.createElement('img')
+  const titreh4 = document.createElement('h4')
+  image.src = movie.imagesURL
+  titreh4.innerText = movie.title
+  mainEl.appendChild(newDiv)
+  newDiv.appendChild(image)
+  newDiv.appendChild(titreh4)
+  newDiv.onclick = afficheInfos.bind(movie)
+}
+
+function messErreur (error) {
   const newP = document.createElement('p')
-  newP.innerText = "Grosse error"
+  newP.innerText = error
   mainEl.appendChild(newP)
-})//Si a Request echoue retourne l'error en console
+}
+
+async function callMyPomise() {
+  try {
+    let response = await fetch('data/movies.json')
+    const movies = await response.json();
+
+    movies.films.forEach(movie => { //forEach de les data que l'on récupère
+    init(movie)
+  })
+  } catch (error) {
+    messErreur(error)
+  }
+}
+
+callMyPomise()
